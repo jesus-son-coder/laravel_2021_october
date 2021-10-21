f2c8d8930344f4747117082a5db7c0a330b8434d
hseka <hseka@lescollectionneurs.com>
2021-10-21 04:49:19 +0200

    Initialisation de l'application

---
+++ laravel-learning-october-2021/app/Http/Controllers/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+
+        echo config('constants.OWT_APP');
+
+        echo config('constants.OWT_KEY');
+
+        echo config('constants.OWT_SECRET');
+
+    }
+}
---
+++ laravel-learning-october-2021/config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'OWT_APP' => 'ONLINE WEB TUTOR APPLICATION',
+    'OWT_KEY' => 'xxxx-xxx',
+    'OWT_SECRET' => 'xxxxxxx-xxxxx-xxxx'
+];
+
